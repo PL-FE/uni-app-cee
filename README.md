183cc0c8d447db460195affb93467103d3133ac8
pengliang <624151713@qq.com>
2025-02-20 11:25:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 开始
+
+框架模板：https://github.com/feige996/unibest
+
+## 设计
+
+750px = 750rpx 1:1, 例如设计稿100px=100rpx
+375px = 750rpx 1:2, 例如设计稿100px=200rpx
---
+++ src/pages/like/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <view>22</view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
